d8b38e4d45580e0b9b21c6c58f7b16f5eaaa68e3
francis85a <franperezcid85@gmail.com>
2025-12-10 10:41:30 +0100

    refactor(word_spining): :recycle: Refactorizar codigo y Añadir casos test
    Refactorizamos el codigo para que sea programacion funcional

--- word_spining.py
+++ word_spining.py
@@ -1,9 +1,13 @@
 def spin_words(words):
-    result = ""
-    
-    for word in words.split():
-        if len(word) >= 5:
-            result += word[::-1] + " "
-        else:
-            result += word + " "
-    return result.strip()
+
+    return " ".join(word[::-1] if len(word) >= 5 else word for word in words.split())
+
+
+if __name__ == "__main__":
+
+    assert spin_words("Hey fellow warriors") == "Hey wollef sroirraw"
+    assert spin_words("This is another test") == "This is rehtona test"
+    assert spin_words("Welcome") == "emocleW"
+    assert spin_words("to") == "to"
+    assert spin_words("CodeWars") == "sraWedoC"
+    print("Pasa los casos test")
